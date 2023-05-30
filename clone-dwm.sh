9bc2b885cbe1e08ae82bb481a8374523d7796c72
Darrell ChinOn <dchinon@yahoo.com>
2023-05-30 22:09:00 +0200

    initial

---
+++ clone-dwm.sh
@@ -0,0 +1,21 @@
+sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/slock
+
+
+Source: https://lecorbeausvault.wordpress.com/2020/04/08/install-guide-suckless-dwm-with-st-and-dmenu/
+cd dwm-*.*
+make
+sudo make clean install
+cd ..
+cd st-*.*
+make
+sudo make clean install
+cd ..
+cd dmenu-*.*
+make
+sudo make clean install
+cd
