d3ec3b81949132710b80c865b395de8aaf1245dd
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-06-08 13:19:37 +0200

    Add some fonts

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+# Update xbps
 sudo xbps-install -Suy
 sudo xbps-install -u xbps
+
+# Add repositories and drivers/firmware
 sudo xbps-install -Suy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree mesa-dri
 sudo xbps-install -Suy intel-ucode
-sudo xbps-install -Suy tlp powertop socklog-void git seatd wayland wlroots noto-fonts-emoji noto-fonts-emoji 
-sudo xbps-install -Suy xdg-user-dirs xdg-utils xdg-desktop-portal-wlr alsa-utils pipewire alsa-pipewire
 
-sudo dracut --regenerate --force
+# Add some utils
+sudo xbps-install -Suy tlp powertop socklog-void git seatd
+sudo xbps-install -Suy wayland wlroots sway waybar
+sudo xbps-install -Suy xdg-user-dirs xdg-utils xdg-desktop-portal-wlr
+sudo xbps-install -Suy alsa-utils pipewire alsa-pipewire
+
+# Add some fonts
+sudo xbps-install -Suy noto-fonts-emoji noto-fonts-emoji otf-font-awesome nerd-fonts-otf
 
 sudo usermod -aG _seatd ${USER}
 sudo usermod -aG socklog ${USER}
@@ -25,4 +33,6 @@ echo "modprobe -r bcm5974" | sudo tee -a /etc/rc.local > /dev/null
 echo "modprobe bcm5974" | sudo tee -a /etc/rc.local > /dev/null
 echo "modprobe powertop --autotune" | sudo tee -a /etc/rc.local > /dev/null
 
+sudo xbps-reconfigure -f fontconfig
+sudo dracut --regenerate --force
 sudo reboot
