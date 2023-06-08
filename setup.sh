51a94d4cf255391a00a5054a6108712ff7506ce8
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-06-08 11:15:49 +0200

    add more libs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 sudo xbps-install -Suy
 sudo xbps-install -u xbps
-sudo xbps-install -Suy void-repo-nonfree tlp powertop mesa-dri socklog-void git seatd wayland wlroots base-devel wlroots-devel 
+sudo xbps-install -Suy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree mesa-dri
 sudo xbps-install -Suy intel-ucode
+sudo xbps-install -Suy tlp powertop socklog-void git seatd wayland wlroots noto-fonts-emoji noto-fonts-emoji 
+sudo xbps-install -Suy xdg-user-dirs xdg-utils xdg-desktop-portal-wlr alsa-utils pipewire alsa-pipewire
 
 sudo dracut --regenerate --force
 
@@ -12,6 +14,8 @@ sudo usermod -aG socklog ${USER}
 sudo ln -s /etc/sv/tlp /var/service
 sudo ln -s /etc/sv/socklog-unix /var/service
 sudo ln -s /etc/sv/seatd /var/service
+sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+sduo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
 
 mkdir -p /home/${USER}/.dwl
 echo "export XDG_RUNTIME_DIR=/home/${USER}/.dwl" >> /home/${USER}/.bashrc
