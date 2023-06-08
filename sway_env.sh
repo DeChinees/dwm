560176e4e0005640736ad8211e84a618237fd7a6
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-06-08 21:35:46 +0200

    Create sway_env.sh

---
+++ sway_env.sh
@@ -0,0 +1,19 @@
+# Session XDG specification
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
+export DESKTOP_SESSION=sway
+
+# XKB
+export XKB_DEFAULT_LAYOUT=us
+
+#export LIBSEAT_BACKEND=logind
+
+# QT
+#QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+#QT_QPA_PLATFORM=wayland-egl
+
+#MOZ_ENABLE_WAYLAND=1
+export CLUTTER_BACKEND=wayland
+export SDL_VIDEODRIVER=wayland
+export WLR_DRM_DEVICES=/dev/dri/card0
