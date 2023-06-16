21a197ac9dcd9bcda1ff7e7806d72d22503a6ff6
Darrell Chin On <darrellc@linux-air.home.lan>
2023-06-16 22:56:53 +0200

    change from sway to budgie-de

--- setup.sh
+++ setup.sh
@@ -8,22 +8,29 @@ sudo xbps-install -Suy void-repo-nonfree void-repo-multilib void-repo-multilib-n
 sudo xbps-install -Suy intel-ucode
 
 # Add some utils
-sudo xbps-install -Suy tlp powertop socklog-void git seatd xorg-fonts
-sudo xbps-install -Suy wayland wlroots sway swayidle swayimg swaylock dmenu foot qutebrowser
-sudo xbps-install -Suy xdg-user-dirs xdg-utils xdg-desktop-portal-wlr
+sudo xbps-install -Suyx tlp tlp-rdw powertop socklog-void git xorg-fonts networkmanager
+sudo xbps-install -Suy dbus elogind xorg budgie-desktop gdm gnome-control-center  
+sudo xbps-install -Suy xdg-user-dirs-gtk xdg-utils xdg-desktop-portal-wlr
 sudo xbps-install -Suy alsa-utils pipewire alsa-pipewire
 
 # Add some fonts
-sudo xbps-install -Suy noto-fonts-emoji noto-fonts-emoji otf-font-awesome nerd-fonts-otf
+sudo xbps-install -Suy noto-fonts-tff noto-fonts-tff-extra noto-fonts-emoji otf-font-awesome nerd-fonts-otf
 
-sudo usermod -aG _seatd ${USER}
+# Set priveledges
 sudo usermod -aG socklog ${USER}
 
+# Enable services
 sudo ln -s /etc/sv/tlp /var/service
 sudo ln -s /etc/sv/socklog-unix /var/service
-sudo ln -s /etc/sv/seatd /var/service
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/elogind /var/service
+sudo ln -s /etc/sv/gdm /var/service
 sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
-sduo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
+sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
+sudo ln -s /usr/sv/networkmanager /var/service
+
+# Disable services
+sudo rm /var/service/dhcpcd
 
 echo "export XDG_RUNTIME_DIR=/tmp" >> /home/${USER}/.bashrc
 
