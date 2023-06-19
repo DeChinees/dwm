b2e601cd8cd4ae73c72f3d3589c59bfd35897072
Darrell Chin On <darrell@cratis.nl>
2023-06-19 20:27:27 +0200

    change to gnome

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo xbps-install -Suy intel-ucode
 
 # Add some utils
 sudo xbps-install -Suy tlp tlp-rdw powertop socklog-void git xorg-fonts NetworkManager
-sudo xbps-install -Suy dbus elogind xorg budgie-desktop gdm gnome-control-center  
+sudo xbps-install -Suy dbus elogind xorg gdm gnome gnome-apps gnome-control-center  
 sudo xbps-install -Suy xdg-user-dirs-gtk xdg-utils xdg-desktop-portal-wlr
 sudo xbps-install -Suy alsa-utils pipewire alsa-pipewire
 
@@ -20,14 +20,14 @@ sudo xbps-install -Suy noto-fonts-tff noto-fonts-tff-extra noto-fonts-emoji otf-
 sudo usermod -aG socklog ${USER}
 
 # Enable services
+sudo ln -s /usr/sv/NetworkManager /var/service
 sudo ln -s /etc/sv/tlp /var/service
 sudo ln -s /etc/sv/socklog-unix /var/service
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/elogind /var/service
-sudo ln -s /etc/sv/gdm /var/service
 sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
-sudo ln -s /usr/sv/NetworkManager /var/service
+sudo ln -s /etc/sv/gdm /var/service
 
 # Disable services
 sudo rm /var/service/dhcpcd
