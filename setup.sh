fcbe8d72d700d7c4103165e84684f866d02603b8
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-06-20 16:07:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # Update xbps
 sudo xbps-install -Suy
 sudo xbps-install -u xbps
@@ -8,8 +9,8 @@ sudo xbps-install -Suy void-repo-nonfree void-repo-multilib void-repo-multilib-n
 sudo xbps-install -Suy intel-ucode
 
 # Add some utils
-sudo xbps-install -Suy tlp tlp-rdw powertop socklog-void git xorg-fonts NetworkManager
-sudo xbps-install -Suy dbus elogind xorg gdm gnome gnome-apps gnome-control-center  
+sudo xbps-install -Suy tlp tlp-rdw powertop socklog-void dbus elogind brightnessctl NetworkManager
+sudo xbps-install -Suy xorg xorg-fonts gdm gnome gnome-apps gnome-control-center gnome-shell-extensions
 sudo xbps-install -Suy xdg-user-dirs-gtk xdg-utils xdg-desktop-portal-wlr
 sudo xbps-install -Suy alsa-utils pipewire alsa-pipewire
 
@@ -18,6 +19,9 @@ sudo xbps-install -Suy noto-fonts-tff noto-fonts-tff-extra noto-fonts-emoji otf-
 
 # Set priveledges
 sudo usermod -aG socklog ${USER}
+
+# Remove redundant utils
+sudo xbps-remove dhcpcd wpa_supplicant
 
 # Enable services
 sudo ln -s /usr/sv/NetworkManager /var/service
@@ -30,10 +34,13 @@ sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
 sudo ln -s /etc/sv/gdm /var/service
 
 # Disable services
+sudo rm /var/service/wpa_supplicant
 sudo rm /var/service/dhcpcd
 
+# Misc. settings
 echo "export XDG_RUNTIME_DIR=/tmp" >> /home/${USER}/.bashrc
 
+# Install drivers
 echo "modprobe -r usbmouse" | sudo tee -a /etc/rc.local > /dev/null 
 echo "modprobe -r bcm5974" | sudo tee -a /etc/rc.local > /dev/null
 echo "modprobe bcm5974" | sudo tee -a /etc/rc.local > /dev/null
