2cc3ef18aca1afb4e576e7a1495fe433d9aa094a
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-10-16 14:52:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo xbps-install -Suy intel-ucode
 # Add some utils
 sudo xbps-install -Suy tlp tlp-rdw powertop socklog-void dbus elogind brightnessctl NetworkManager bluez preload rsync kate git
 sudo xbps-install -Suy xorg-minimal xorg-input-drivers xorg-video-drivers xorg-fonts setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-utils pipewire alsa-pipewire
-sudo xbps-install -Suy kde5 kde5-baseapps firefox-esr
+sudo xbps-install -Suy kde5 kde5-baseapps sddm firefox-esr
 sudo xbps-install -Suy xdg-user-dirs xdg-utils xdg-desktop-portal-wlr xtools micro
 
 # Add some fonts
@@ -23,12 +23,12 @@ sudo usermod -aG socklog ${USER}
 # Enable services
 sudo ln -s /etc/sv/networkmanager /var/service
 sudo ln -s /etc/sv/tlp /var/service
+sudo ln -s /etc/sv/sddm /var/service
 sudo ln -s /etc/sv/socklog-unix /var/service
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/elogind /var/service
 sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
-sudo ln -s /etc/sv/lxdm /var/service
 
 # Misc. settings
 echo "export XDG_RUNTIME_DIR=/tmp" >> /home/${USER}/.bashrc
