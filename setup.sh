3bfb2cf2be091ae1e03d7d58dc7622f575d744cc
DeChinees <63873884+DeChinees@users.noreply.github.com>
2023-10-16 12:44:22 +0200

    do note delete dhcpd and wpasup.

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ sudo xbps-install -Suy intel-ucode
 
 # Add some utils
 sudo xbps-install -Suy tlp tlp-rdw powertop socklog-void dbus elogind brightnessctl NetworkManager
-sudo xbps-install -Suy xorg-minimal xorg-input-drivers xorg-video-drivers xorg-fonts setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-utils pipewire alsa-pipewire"
-sudo xbps-install -Suy lxdm enlightenment terminology econnman udisks2 firefox-esr"
+sudo xbps-install -Suy xorg-minimal xorg-input-drivers xorg-video-drivers xorg-fonts setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-utils pipewire alsa-pipewire
+sudo xbps-install -Suy lxdm enlightenment terminology econnman udisks2 firefox-esr
 sudo xbps-install -Suy xdg-user-dirs-gtk xdg-utils xdg-desktop-portal-wlr
 
 # Add some fonts
@@ -20,11 +20,7 @@ sudo xbps-install -Suy noto-fonts-tff noto-fonts-tff-extra noto-fonts-emoji otf-
 # Set priveledges
 sudo usermod -aG socklog ${USER}
 
-# Remove redundant utils
-sudo xbps-remove dhcpcd wpa_supplicant
-
 # Enable services
-sudo ln -s /usr/sv/NetworkManager /var/service
 sudo ln -s /etc/sv/tlp /var/service
 sudo ln -s /etc/sv/socklog-unix /var/service
 sudo ln -s /etc/sv/dbus /var/service
@@ -32,10 +28,6 @@ sudo ln -s /etc/sv/elogind /var/service
 sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
 sudo ln -s /etc/sv/lxdm /var/service
-
-# Disable services
-sudo rm /var/service/wpa_supplicant
-sudo rm /var/service/dhcpcd
 
 # Misc. settings
 echo "export XDG_RUNTIME_DIR=/tmp" >> /home/${USER}/.bashrc
